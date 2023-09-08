6fc06ecc2d72931cd3465e48bdb8939f35c9d90c
Salvador <salvadorprogramador@gmail.com>
2023-09-08 15:51:30 -0500

    Edad mejorada
    Actualización menor:
    -Solución de errores
    -Mejorada la función de pedir edad

--- proyectos/encuesta.js
+++ proyectos/encuesta.js
@@ -1,20 +1,35 @@
-function hablar () {
-    // Obtén la referencia al elemento <h2> con el id "mensaje2"
-    var nombre = prompt("Ingrese su nombre");
+function hablar() {
+    // Solicitar al usuario que ingrese su nombre
+    let nombre = prompt("Ingrese su nombre");
     let mensaje2 = document.getElementById("mensaje2");
 
+    // Verificar si el nombre contiene letras (con tildes), la letra "ñ" y espacios
+    if (!/^[\w\sáéíóúÁÉÍÓÚñÑçÇýÝ]+$/.test(nombre)) {
+        alert('El nombre no puede contener símbolos ni números. Por favor ingrese un nombre válido.');
+        hablar(); // Llamar recursivamente si no es un nombre válido
+        return; // Salir de la función para evitar que continúe
+    }
 
-    // Cambia el contenido del elemento <h2>
-    mensaje2.textContent =  "¡Hola " + nombre + "!";
+    // Cambiar el contenido del elemento <h2>
+    mensaje2.textContent = "¡Hola " + nombre + "!";
 
-    // Obtén la referencia al elemento <h3> con el id "mensaje3"
-    var nacimiento = prompt("Ingrese su año de nacimiento");
+    // Solicitar al usuario que ingrese su fecha de nacimiento
+    let fechaNacimiento = prompt("Ingrese su fecha de nacimiento (AAAA-MM-DD)");
     let mensaje3 = document.getElementById("mensaje3");
-    //Año actual
-    var anioActual = new Date().getFullYear();
-    var edad = anioActual - nacimiento
-    
-    // Cambia el contenido del elemento <h3>
-    mensaje3.textContent = "Tienes " + edad + "  años";
 
+    // Verificar si la fecha de nacimiento es un formato válido
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(fechaNacimiento)) {
+        alert('Por favor ingrese una fecha de nacimiento válida en el formato AAAA-MM-DD.');
+        hablar(); // Llamar recursivamente si no es una fecha válida
+        return; // Salir de la función para evitar que continúe
+    }
+
+    // Calcular la edad en años
+    let fechaActual = new Date();
+    let fechaNac = new Date(fechaNacimiento);
+    let edadEnMillisegundos = fechaActual - fechaNac;
+    let edad = Math.floor(edadEnMillisegundos / 31536000000); // 1 año en milisegundos
+
+    // Cambiar el contenido del elemento <h3> con la edad calculada
+    mensaje3.textContent = "Tienes " + edad + " años.";
 }
