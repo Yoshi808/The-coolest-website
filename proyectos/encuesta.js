e73937c787e6dd583ab0600c769f247e1e8799fc
Salvador <salvadorprogramador@gmail.com>
2023-09-08 16:36:53 -0500

    Solución de nuevos errores
    Actualización menor:
    -En la actualización anterior se crearon nuevos errores que ya fueron solucionados

--- proyectos/encuesta.js
+++ proyectos/encuesta.js
@@ -1,3 +1,18 @@
+function calcularEdad(fechaNacimiento) {
+    let fechaActual = new Date();
+    let nacimiento = new Date(fechaNacimiento);
+
+    let anios = fechaActual.getFullYear() - nacimiento.getFullYear();
+    let meses = fechaActual.getMonth() - nacimiento.getMonth();
+    let dias = fechaActual.getDate() - nacimiento.getDate();
+
+    if (meses < 0 || (meses === 0 && dias < 0)) {
+        anios--;
+    }
+
+    return anios;
+}
+
 function hablar() {
     // Solicitar al usuario que ingrese su nombre
     let nombre = prompt("Ingrese su nombre");
@@ -14,21 +29,35 @@ function hablar() {
     mensaje2.textContent = "¡Hola " + nombre + "!";
 
     // Solicitar al usuario que ingrese su fecha de nacimiento
-    let fechaNacimiento = prompt("Ingrese su fecha de nacimiento (AAAA-MM-DD)");
     let mensaje3 = document.getElementById("mensaje3");
+    let fechaNacimiento;
 
-    // Verificar si la fecha de nacimiento es un formato válido
-    if (!/^\d{4}-\d{2}-\d{2}$/.test(fechaNacimiento)) {
-        alert('Por favor ingrese una fecha de nacimiento válida en el formato AAAA-MM-DD.');
-        hablar(); // Llamar recursivamente si no es una fecha válida
-        return; // Salir de la función para evitar que continúe
+    while (true) {
+        let fechaActual = new Date();
+        let fechamin = new Date(fechaActual);
+        fechamin.setFullYear(fechaActual.getFullYear() - 200);
+
+        fechaNacimiento = prompt("Ingrese su fecha de nacimiento (AAAA-MM-DD)");
+
+        // Verificar si la fecha de nacimiento es un formato válido
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(fechaNacimiento)) {
+            alert('Por favor ingrese una fecha de nacimiento válida en el formato AAAA-MM-DD.');
+        } else {
+            let nacimiento = new Date(fechaNacimiento);
+
+            // Verificar si la fecha de nacimiento es mayor que la fecha actual
+            if (nacimiento > fechaActual) {
+                alert('La fecha de nacimiento no puede ser mayor que la fecha actual.');
+            } else if (nacimiento < fechamin) {
+                alert('Ingrese una fecha de nacimiento válida (' + fechamin.getFullYear() + '-' + fechaActual.getFullYear() + ").");
+            } else {
+                break; // Salir del bucle si la fecha es válida
+            }
+        }
     }
 
-    // Calcular la edad en años
-    let fechaActual = new Date();
-    let fechaNac = new Date(fechaNacimiento);
-    let edadEnMillisegundos = fechaActual - fechaNac;
-    let edad = Math.floor(edadEnMillisegundos / 31536000000); // 1 año en milisegundos
+    // Calcular la edad de manera precisa
+    let edad = calcularEdad(fechaNacimiento);
 
     // Cambiar el contenido del elemento <h3> con la edad calculada
     mensaje3.textContent = "Tienes " + edad + " años.";
